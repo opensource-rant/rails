3c373e8e642e947a2b8cc739b59060ec38c081e1
Richard Anthony <public.rant@pm.me>
2022-04-14 15:27:09 +1000

    Test web extension

---
+++ bookmarks.test.js
@@ -0,0 +1,24 @@
+// const bookmarks = require('./app/scripts/bookmarks.js')
+// reads open tab groups
+// chrome.bookmarks.create = jest.fn()
+// reads history
+// chrome.history = jest.fn()
+// https://developer.chrome.com/docs/extensions/reference/tabGroups/#type-TabGroup
+// https://developer.chrome.com/docs/extensions/reference/tabs/#type-Tab
+// Promise {<pending>}
+// [[Prototype]]: Promise
+// [[PromiseState]]: "fulfilled"
+// [[PromiseResult]]: Object
+// collapsed: true
+// color: "orange"
+// id: 281409528
+// title: "reactjs"
+// windowId: 2998
+// [[Prototype]]: Object
+it('creates a bookmark for each tab', () => {
+  expect(true).toBe(true)
+})
+it('includes details about the tab group in the URL')
+// sends data downstream
+// invokes tensor.js
+it('uses machine learning to group large amounts of info based on the training set (tabGroups)')
