ef425a6fa6defc26b3464605e9bef0c278c9dc55
Richard Anthony <public.rant@pm.me>
2022-06-05 21:19:34 +1000

    Backgorund

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -8,3 +8,12 @@
 // });
 // document.body.style.backgroundColor="red"
 console.log(`'Allo 'Allo! Event Page`)
+// this is the background code...
+//
+// // listen for our browerAction to be clicked
+// chrome.browserAction.onClicked.addListener(function (tab) {
+//  // for the current tab, inject the "inject.js" file & execute it
+//    chrome.tabs.executeScript(tab.ib, {
+//        file: 'inject.js'
+//          });
+//          });
