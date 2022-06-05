62df9768c37f2c20fffe9a6a1652dd830fe03ad7
Richard Anthony <public.rant@pm.me>
2022-06-05 21:20:39 +1000

    inject

---
+++ app/scripts/inject.js
@@ -0,0 +1,13 @@
+console.log("here");
+
+
+const oReq = new XMLHttpRequest();
+// oReq.open("GET", "http://www.google.com");
+// oReq.send().then((result) => {
+//   console.log(result);
+// }).catch((err) => {
+//   console.error(err);
+// });
+//
+
+console.log(oReq);
